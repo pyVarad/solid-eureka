c6f690310362fbfd2228eb8e7822676d6956dab4
Varad <py.varad@gmail.com>
2017-03-02 06:37:32 +0530

    problem -1

---
+++ projectEuler/problem-1/solution_1.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+'''
+if we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.Find the sum of all
+the multiples of 3 or 5 below 1000.
+'''
+
+from commonLibs.timer import Timer
+
+@profile
+def solution(limit):
+    """
+    Find the numbers divisible by 3 and 5 and add them
+    """
+    sum = 0
+    for num in range(1, limit):
+        if num % 3 == 0 or num % 5 == 0:
+            sum += num
+
+    return sum
+
+
+@profile
+def solution_effective(limit, divisor):
+    """
+    Effective Algorithm
+    """
+    target = (limit-1) // divisor
+    sum_of_all = divisor * (target * (target + 1))/2
+    return sum_of_all
+
+
+if __name__ == "__main__":
+    limit = 1000
+    with Timer() as T:
+        print solution(limit)
+    print "Completed in %f" % T.timeElapsed
+
+    with Timer() as T:
+        result = solution_effective(limit, 3) + \
+                solution_effective(limit, 5) -  \
+                solution_effective(limit, 15)
+        print result
+    print "Completed in %f" % T.timeElapsed
