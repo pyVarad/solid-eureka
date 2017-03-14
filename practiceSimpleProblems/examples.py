dc4d84d01c479947b64eaf0a8a33d7022a97837a
Varad <py.varad@gmail.com>
2017-03-14 05:44:32 +0530

    simple recursion

---
+++ practiceSimpleProblems/examples.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+FibanocciSeries = [1]
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+       return n*factorial(n-1)
+
+
+def fibanocci(a=1, b=1, limit=100000000000000000000000000000000000000000000000000):
+    if limit > b:
+       a, b = b, a+b
+       FibanocciSeries.append(a)
+       return fibanocci(a, b, limit-1)
+
+
+if __name__ == "__main__":
+   fibanocci()
+   print FibanocciSeries
